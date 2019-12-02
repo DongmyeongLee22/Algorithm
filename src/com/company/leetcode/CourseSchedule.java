417bda973a92b772405f08320f46bc1be59121a9
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-02 21:25:28 +0900

    리트코드 문제 추가

---
+++ src/com/company/leetcode/CourseSchedule.java
@@ -0,0 +1,39 @@
+package com.company.leetcode;
+
+import java.util.*;
+
+public class CourseSchedule {
+    class Solution {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            int[] indegree = new int[numCourses];
+            List<List<Integer>> ret = new ArrayList<>();
+            for (int i = 0; i < numCourses; i++) {
+                ret.add(new ArrayList<Integer>());
+            }
+
+            for (int[] num : prerequisites) {
+                indegree[num[0]]++;
+                ret.get(num[1]).add(num[0]);
+            }
+
+            Queue<Integer> q = new LinkedList<>();
+
+            for (int i = 0; i < numCourses; i++) {
+                if (indegree[i] == 0) q.offer(i);
+            }
+            Set<Integer> hashSet = new HashSet<>();
+
+            while (!q.isEmpty()) {
+                int val = q.poll();
+                List<Integer> list = ret.get(val);
+                hashSet.add(val);
+
+                for (Integer i : list) {
+                    if (--indegree[i] == 0) q.offer(i);
+                }
+            }
+
+            return numCourses == hashSet.size();
+        }
+    }
+}
